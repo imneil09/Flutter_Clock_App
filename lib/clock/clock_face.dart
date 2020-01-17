66d7f4e636da0d4446405485222d74852b95f9d9
iamneil0.9 <iamneil@gmail.com>
2020-01-18 03:21:17 +0530

    simplified design of the clock

--- lib/clock/clock_face.dart
+++ lib/clock/clock_face.dart
@@ -1,13 +1,11 @@
 import 'clock_text.dart';
 import 'package:flutter/material.dart';
-import 'clock_dial_painter.dart';
-import 'clock_hands.dart';
 
 class ClockFace extends StatelessWidget{
 
   final DateTime dateTime;
   final ClockText clockText;
-  ClockFace({this.clockText = ClockText.arabic, this.dateTime});
+  ClockFace({this.clockText = ClockText.sanskrit, this.dateTime});
 
   @override
   Widget build(BuildContext context) {
--- lib/clock/clock_text.dart
+++ lib/clock/clock_text.dart
@@ -1,4 +1,4 @@
 enum ClockText{
+  sanskrit,
   roman,
-  arabic
 }
